66a5464e39e6c4352326e64dc840462e4ca6451e
kkan0615 <kkan0615@gmail.com>
2023-05-08 21:01:09 -0400

    :zap: Feat: chang file name

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+/* Router */
 import { HashRouter } from 'react-router-dom'
 /* Install i18n-next module */
 import './i18n'
 import { Provider } from 'react-redux'
 import store from './stores'
 import './index.scss'
-// Chakra UI
+/* Chakra UI */
 import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from '@renderer/utils/libs/Chakra'
+import { theme } from '@renderer/utils/libs/chakra'
+/* App */
 import App from './App'
+/*Tour component */
 import { TourProvider } from '@reactour/tour'
 import TourCloseBtn from '@renderer/components/buttons/TourClose'
-
+// Default Step
 const steps: any[] = []
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
